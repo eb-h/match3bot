b3ad01d9bd38c1ad8d0dba30117987428f7debaa
Eric <u5586464@anu.edu.au>
2015-08-18 07:53:42 +1000

    Dead code removed, mouse now moves at a constant speed so it isn't annoyingly slow for small movements and too fast to register for far movements

--- src/ioFacade.py
+++ src/ioFacade.py
@@ -3,22 +3,8 @@ from pykeyboard import PyKeyboard
 from gameData import xCoords, yCoords, originX, originY
 import time
 import pyautogui
+import math
 
-'''
-def mouseDrag(origin=(0,0), dest=(0,0), sleepTime=ioPauseTime):
-    """ 
-        sleeptime necessary or it's too fast for 
-        linux to register a change 
-    """
-    m = PyMouse()
-    m.move(origin[0], origin[1])
-    time.sleep(sleepTime)
-    m.press(origin[0], origin[1])
-    time.sleep(sleepTime)
-    m.move(origin[0], origin[1])
-    time.sleep(sleepTime)
-    m.release(dest[0], dest[1])
-'''
 
 def getTilePosition((tileX, tileY), xCoords=xCoords, yCoords=yCoords):
     """
@@ -27,12 +13,6 @@ def getTilePosition((tileX, tileY), xCoords=xCoords, yCoords=yCoords):
     """
     return (originX+xCoords[tileX], originY+yCoords[tileY])
 
-
-'''
-def mouseDragByTile(tile1Pos, tile2Pos):
-    mouseDrag(getTilePosition(tile1Pos),
-            getTilePosition(tile2Pos))
-'''
 
 def switchFocus(n=1, sleepTime=0.1):
     """ Performs alt tab, can skip n-1 applications """
@@ -45,6 +25,16 @@ def switchFocus(n=1, sleepTime=0.1):
     k.release_key(k.alt_key)
 
 
+def distance(p0, p1):
+    return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
+
+
+def speedOfMouseMovement(origin, dest, unitSpeed=0.002):
+    return distance(origin, dest) * unitSpeed
+
+
 def autoGuiMouseDrag(origin, dest):
     pyautogui.moveTo(origin[0], origin[1])
-    pyautogui.dragTo(dest[0], dest[1], 1, button='left')
+    pyautogui.dragTo(dest[0], dest[1], 
+            speedOfMouseMovement(origin, dest), 
+            button='left')
